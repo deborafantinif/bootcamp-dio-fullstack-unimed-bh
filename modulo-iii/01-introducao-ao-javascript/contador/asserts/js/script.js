4e6516253d61a86c40b315b42fb0dff30a0df2d2
deborafantinif <deboraferfan@gmail.com>
2022-06-16 11:49:57 -0300

    fear: add addEventListener and condition in counter

--- modulo-iii/01-introducao-ao-javascript/contador/asserts/js/script.js
+++ modulo-iii/01-introducao-ao-javascript/contador/asserts/js/script.js
@@ -1,12 +1,28 @@
 const currentNumberWrapper = document.getElementById('currentNumber');
+const buttonDecrement = document.getElementsByName('sub')[0];
+const buttonIncrement = document.getElementsByName('sum')[0];
 let currentNumber = 0;
 
+
 const decrement = () => {
+  if (currentNumber === -10) return buttonDecrement.disable = true
   currentNumber--
   currentNumberWrapper.innerHTML = currentNumber;
+  if (currentNumber < 0) return currentNumberWrapper.style.color = 'red'
 }
 
+buttonDecrement.addEventListener('click', () => {
+  decrement()
+});
+
 const increment = () => {
+  if (currentNumber === 10) return buttonDecrement.disable = true
   currentNumber++
   currentNumberWrapper.innerHTML = currentNumber;
+  if (currentNumber > 0) return currentNumberWrapper.style.color = 'black'
 }
+
+buttonIncrement.addEventListener('click', () => {
+  increment()
+});
+
